7ef9d37c551f308a7a63a5e4e2456fa25cf4b47f
j4games4ev@gmail.com <j4games4ev@gmail.com>
2019-01-26 20:17:39 +0530

    26/1/19

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,11 +5,38 @@ using UnityEngine.SceneManagement;
 
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.UI;
 
 namespace com.j4games.rndMultiplayer
 {
     public class GameManager : MonoBehaviourPunCallbacks
     {
+
+        #region Public Fields
+
+        [Tooltip("The prefab to use for representing the player")]
+        public GameObject playerPrefab;
+
+        #endregion
+
+        public static GameManager Instance;
+
+        void Start()
+        {
+            Instance = this;
+
+            if (playerPrefab == null)
+            {
+                Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+            }
+            else
+            {
+                Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+                // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+                PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+            }
+        }
+
         #region Photon CallBacks
 
         /// <summary>
@@ -42,15 +69,14 @@ namespace com.j4games.rndMultiplayer
             Debug.LogFormat("PhotonNetwork : Loading Level : {0}", PhotonNetwork.CurrentRoom.PlayerCount);
             PhotonNetwork.LoadLevel("Room for " + PhotonNetwork.CurrentRoom.PlayerCount);
         }
+
         #endregion
 
         #region Photon Callbacks
 
-
         public override void OnPlayerEnteredRoom(Player other)
         {
             Debug.LogFormat("OnPlayerEnteredRoom() {0}", other.NickName); // not seen if you're the player connecting
-
 
             if (PhotonNetwork.IsMasterClient)
             {
@@ -69,7 +95,6 @@ namespace com.j4games.rndMultiplayer
             if (PhotonNetwork.IsMasterClient)
             {
                 Debug.LogFormat("OnPlayerLeftRoom IsMasterClient {0}", PhotonNetwork.IsMasterClient); // called before OnPlayerLeftRoom
-
 
                 LoadArena();
             }
